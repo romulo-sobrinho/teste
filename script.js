8b4c3bf8d58f6b7bbe187fdc1ad9351548d228f1
Romulo <romulosilva001@gmail.com>
2022-03-16 15:10:49 -0300

    Alteração disposição section confirmation na tela mob

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let btnConfirmation = document.querySelector("#btn-confirmation");
-
-
-function toggleConfirmation() {
+function toggleConfirmationPayment() {
   let sectionConfirmation = document.querySelector(".section__confirmation");
   sectionConfirmation.classList.toggle("active");
   let sectionPayment = document.querySelector(".section__payment");
   sectionPayment.classList.toggle("active");
 }
 
+let btnPayment = document.querySelector("#btn-payment");
+btnPayment.addEventListener("click", toggleConfirmationPayment);
 
+
+
+function toggleConfirmation() {
+  let sectionConfirmation = document.querySelector(".section__confirmation");
+  sectionConfirmation.classList.toggle("active");
+}
+
+let btnConfirmation = document.querySelector("#btn-confirmation");
 btnConfirmation.addEventListener("click", toggleConfirmation);
-
-
-
 
 
